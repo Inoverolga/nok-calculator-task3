88c40d8c4b9422a97740c0b3741e692813b772a6
Inover Olga <kuzma-inoverolga@mail.ru>
2025-09-24 21:44:13 +0300

    fix: добавила проверку final1 )

--- netlify/functions/nok.js
+++ netlify/functions/nok.js
@@ -3,7 +3,7 @@ export const handler = async (event) => {
     const bigX = BigInt(x);
     const bigY = BigInt(y);
 
-    if (![x, y].every((num) => Number.isInteger(num) && num > 0n)) {
+    if (![+x, +y].every((num) => Number.isInteger(num) && num > 0n)) {
       return "NaN";
     } else {
       function z(a, b) {

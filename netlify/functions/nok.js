d9c03f8261eb520e14f8f9999bfa9ba5218332e2
Inover Olga <kuzma-inoverolga@mail.ru>
2025-09-24 16:21:38 +0300

    fix: исправлена функция НОД (было деление на 0)

--- netlify/functions/nok.js
+++ netlify/functions/nok.js
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
   function HOK(x, y) {
     if (![x, y].every((num) => Number.isInteger(num) && num > 0)) return NaN;
-    const nod = (a, b) => (a === 0 ? a : nod(b, a % b));
+    const nod = (a, b) => (b === 0 ? a : nod(b, a % b));
     return (x * y) / nod(x, y);
   }
 

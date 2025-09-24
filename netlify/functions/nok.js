e960d9ac00728101154ae2488bdfb0b495c374d1
Inover Olga <kuzma-inoverolga@mail.ru>
2025-09-24 21:19:59 +0300

    fix: добавила проверку final1 )

--- netlify/functions/nok.js
+++ netlify/functions/nok.js
@@ -1,32 +1,32 @@
 export const handler = async (event) => {
-  function HOK(x, y) {
-    if (![x, y].every((num) => Number.isInteger(num) && num > 0)) {
-      console.log("NaN");
-      return "NaN";
-    } else {
-      function z(a, b) {
-        while (b !== 0) {
-          let t = b;
-          b = a % b;
-          a = t;
-        }
-        return a;
+  const bigX = BigInt(x);
+  const bigY = BigInt(y);
+
+  if (![x, y].every((num) => Number.isInteger(num) && num > 0n)) {
+    return "NaN";
+  } else {
+    function z(a, b) {
+      while (b !== 0n) {
+        let t = b;
+        b = a % b;
+        a = t;
       }
-      const s = ((x * y) / z(x, y)).toString();
-      return s;
+      return a;
     }
+    const s = ((bigX * bigY) / z(bigX, bigY)).toString();
+    return s;
   }
+};
 
-  const { x, y } = event.queryStringParameters;
+const { x, y } = event.queryStringParameters;
 
-  const result = HOK(+x, +y);
+const result = HOK(+x, +y);
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: result,
-  };
+return {
+  statusCode: 200,
+  headers: {
+    "Content-Type": "text/plain",
+    "Access-Control-Allow-Origin": "*",
+  },
+  body: result,
 };

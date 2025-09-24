65edec6a52474a240245bd1b8ea941a214b0af73
Inover Olga <kuzma-inoverolga@mail.ru>
2025-09-24 18:52:16 +0300

    fix: добавила some)

--- netlify/functions/nok.js
+++ netlify/functions/nok.js
@@ -2,17 +2,8 @@ export const handler = async (event) => {
   function HOK(x, y) {
     if ([x, y].some((num) => num === 0)) return "0";
     if (![x, y].every((num) => Number.isInteger(num) && num > 0)) return "NaN";
-
-    function not(a, b) {
-      while (b !== 0) {
-        let temp = b;
-        b = a % b;
-        a = temp;
-      }
-      return a;
-    }
-    const z = not(x, y);
-    return (x / z) * y;
+    for (let i = Math.max(x, y); i <= x * y; i++)
+      if (i % x === 0 && i % y === 0) return i;
   }
 
   const { x, y } = event.queryStringParameters;

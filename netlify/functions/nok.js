f60d5b6f8754bbf038da328d38957a691ba7cb52
Inover Olga <kuzma-inoverolga@mail.ru>
2025-09-24 21:06:41 +0300

    fix: добавила проверку final )

--- netlify/functions/nok.js
+++ netlify/functions/nok.js
@@ -3,19 +3,18 @@ export const handler = async (event) => {
     if (![x, y].every((num) => Number.isInteger(num) && num > 0)) {
       console.log("NaN");
       return "NaN";
-    }
-
-    function z(a, b) {
-      while (b !== 0) {
-        let t = b;
-        b = a % b;
-        a = t;
+    } else {
+      function z(a, b) {
+        while (b !== 0) {
+          let t = b;
+          b = a % b;
+          a = t;
+        }
+        return a;
       }
-      return a;
+      const s = ((x * y) / z(x, y)).toString();
+      return s;
     }
-    const s = ((x * y) / z(x, y)).toString();
-    console.log(typeof s);
-    return (x * y) / z(x, y);
   }
 
   const { x, y } = event.queryStringParameters;
